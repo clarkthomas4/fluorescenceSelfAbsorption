e6fdeda7c4f81df8de4d6d1d107082fc5931db63
Clark T.J <tjc3g13@soton.ac.uk>
2019-03-28 13:40:36 +0000

    [skip travis] applied linter to tests

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 import unittest
 import fluorescenceSelfAbsorption as fSA
 
+
 class TestFSA(unittest.TestCase):
 
     def test_scan(self):
         scanDataFile = fSA.jsonDataFile("ScanData.json")
         scanData = scanDataFile.getData()
         scan = fSA.scan(scanDataFile)
-        self.assertEqual(scan.getAbsorptionTomo(), scanData["absorptionTomo"]["path"])
+        self.assertEqual(scan.getAbsorptionTomo(),
+                         scanData["absorptionTomo"]["path"])
+
 
 if __name__ == 'main':
     unittest.main()
